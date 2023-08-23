8463ab2b096d81bc7669cc5a82fb75916af33bab
Vintana <vintanafanteneca@gmail.com>
2023-08-24 02:43:51 +0800

    🕵🏽 Updated spyfall.py

--- src/spy_fall.py
+++ src/spy_fall.py
@@ -7,19 +7,24 @@ class spyFall:
     players: list = []
     discordserver = ""
     default = {
-        1 : ["Hospital", "Doctor", "Nurse"],
-        2 : ["Airplane", "Pilot", "Flight Attendant"]
+        "Hospital" : ["Doctor", "Nurse"],
+        "Airplane" : ["Pilot", "Flight Attendant"]
     }
+    currentgame = {}
     def __init__(self, game_host, discordserver):
         self.game_host = game_host
         self.players.append(game_host)
         self.discordserver = discordserver
         if not os.path.exists(f"data\spy_fall\{self.discordserver}.json"):
             with open(f"{self.discordserver}.json", "w") as json_file:
-            json_file.write(json.dumps(self.default, indent=4))
+                json_file.write(json.dumps(self.default, indent=4))
+        if os.path.exists(f"data\spy_fall\{self.discordserver}.json"):
+            with open("data.json", "r") as json_file:
+                self.currentgame = json.load(json_file)
 
     def showplayers(self) -> list:
         return self.players
     
     def addmap(self, map : list):
-        newmap = {number: list}
+        newmap = {map: list}
+        self.currentgame.update(newmap)
